4f0f91b03f532dba48907ecfba3967ac2c610f67
Ashay Aswale <asaswale@wpi.edu>
2020-02-13 21:12:13 -0500

    Works 90% of time with random generated board. Does not work with given board.

--- hill_climb.py
+++ hill_climb.py
@@ -1,53 +1,90 @@
 from common import HeavyQueenCommons
 from copy import copy, deepcopy
+import time
+import random
 import numpy as np
+
 
 class HillClimb:
     def __init__(self, queens, heuristic):
         self.common = HeavyQueenCommons(queens, heuristic)
         self.n = len(queens)
-        
+
         self._local_qn = deepcopy(queens)
         self._local_qn_master = deepcopy(queens)
 
     def solve(self):
+        start_time = time.time()
         looper = True
+        solving = True
+
         start_attack = self.common.getAttackList(self._local_qn)
         lowest_heur = self.common.getHeuristic(start_attack)
-        print ("Attacking Positions at start: ", lowest_heur)
+        start_heur = (lowest_heur)
+        print("Heuristic at start: ", lowest_heur)
+        print("\n \n")
 
-        while looper:
+        # MOVE TO SELF
+        max_shoulder_moves = 100
+        global_lowest_heur = lowest_heur
+        lowest_qn = deepcopy(self._local_qn)
+        moves_tried = 0
 
-            move_log = self.getMoveLog()
+        while solving:
+            shoulder_moves = 0
+            self._local_qn = deepcopy(self._local_qn_master)
+            start_attack = self.common.getAttackList(self._local_qn)
+            lowest_heur = self.common.getHeuristic(start_attack)
 
-            # print("### Move Log ###")
-            # print(move_log)
-            # print("\n")
+            while looper:
+                move_log = self.getMoveLog()
+                if len(move_log) == 0:
+                    solving = False
+                    looper = False
+                    break
 
-            possible_moves = self.common.getLowestHeurMoves(move_log)
-            possible_moves = self.common.getLowestCostMoves(possible_moves)
+                possible_moves = self.common.getLowestHeurMoves(move_log)
+                i = random.randint(0, len(possible_moves)-1)
 
-            # print("### Lowest Heuristic Moves ###")
-            # print(possible_moves)
-            # print("\n")
-            
-            if lowest_heur > possible_moves[0][2]:
-                lowest_heur = possible_moves[0][2]
-                col = possible_moves[0][0]
-                row = possible_moves[0][1]
-                self._local_qn[col][0] = row
-            else:
-                looper = False
-        self.common.drawBoard(self._local_qn)
-        end_attack = self.common.getAttackList(self._local_qn)
+                if lowest_heur >= possible_moves[i][2]:
+                    if lowest_heur == possible_moves[i][2]:
+                        shoulder_moves += 1
+                    lowest_heur = possible_moves[i][2]
+                    col = possible_moves[i][0]
+                    row = possible_moves[i][1]
+                    self._local_qn[col][0] = row
+                    moves_tried += 1
+                else:
+                    looper = False
+
+                if shoulder_moves >= max_shoulder_moves:
+                    break
+
+                elapsed_time = time.time() - start_time
+                if elapsed_time > 10:
+                    solving = False
+                    looper = False
+                    break
+
+            if global_lowest_heur > lowest_heur:
+                global_lowest_heur = lowest_heur
+                lowest_qn = deepcopy(self._local_qn)
+
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 10:
+                break
+
+        self.common.drawBoard(lowest_qn)
+        end_attack = self.common.getAttackList(lowest_qn)
         heur = self.common.getHeuristic(end_attack)
-        print ("Attacking Positions at end: ", heur)
+        print("Attacking Positions at end: ", global_lowest_heur)
 
-
+        print("\n Moves tried:", moves_tried)
 
     """
         Move Log = [queen column, queen moved row, heuristic val, cost]
     """
+
     def getMoveLog(self):
         move_log = []
         attack_list = []
@@ -55,17 +92,20 @@ class HillClimb:
         heur_val = 0
         # for i in range(0,self.n):    # Column Iterator
         attack_list = self.common.getAttackList(self._local_qn)
-        light_qn_col_list = self.common.getLightQnColumn(attack_list)
+        if len(attack_list) == 0:
+            return move_log
+        light_qn_col_list = self.common.getAttackQueenList(attack_list)
         # light_qn_col = np.random.choice(light_qn_col_list)
         for light_qn_col in light_qn_col_list:
-            for j in range(0,self.n):      # Row Iterator
-                if j == self._local_qn[light_qn_col][0]:    # if switch is current position
+            for j in range(0, self.n):      # Row Iterator
+                # if switch is current position
+                if j == self._local_qn[light_qn_col][0]:
                     continue
                 self._local_qn[light_qn_col][0] = j
                 attack_list = self.common.getAttackList(self._local_qn)
                 heur_val = self.common.getHeuristic(attack_list)
                 move_cost = self.common.getMoveCost(light_qn_col, j)
                 move_log.append([light_qn_col, j, heur_val, move_cost])
-                self._local_qn[light_qn_col][0] = self._local_qn_master[light_qn_col][0] 
+                self._local_qn[light_qn_col][0] = self._local_qn_master[light_qn_col][0]
         # print (move_log)
         return np.array(move_log)
--- main.py
+++ main.py
@@ -5,19 +5,20 @@ from array import *
 import numpy as np
 from hill_climb import HillClimb
 
+
 def createBoard(argv):
     """
         Formation:
             Queen = [Column][Row, Weight]
-            
+
     """
     n = int(argv[0])
-    queens = np.zeros([n, 2], dtype = int) # Row of existance and weight
+    queens = np.zeros([n, 2], dtype=int)  # Row of existance and weight
 
-    board = np.zeros([n, n], dtype = int) 
+    board = np.zeros([n, n], dtype=int)
     for i in range(0, n):
-        q_wt = random.randint(1,9)
-        pos = random.randint(0,n-1)
+        q_wt = random.randint(1, 9)
+        pos = random.randint(0, n-1)
         board[i][pos] = q_wt
         queens[i][:] = (pos, q_wt)
 
@@ -28,21 +29,40 @@ def createBoard(argv):
     # print(queens)
     return queens
 
+
+def getDemoBoard():
+    queens = [[4, 9], [3, 3], [4, 1], [2, 4], [1, 2]]
+    n = len(queens)
+    board = np.zeros([n, n], dtype=int)
+    for i in range(0, n):
+        pos = queens[i][0]
+        q_wt = queens[i][1]
+        board[i][pos] = q_wt
+
+    print("\n")
+    print("### Queen Board ###")
+    print(board.T)
+    print("\n")
+    # print(queens)
+    return queens
+
+
 """
     Heuristic = 0 -> H1
                 1 -> H2
 """
+
+
 def main(argv):
     queens = createBoard(argv)
-    heur = 0 if argv[2] == "H1" else 1 
+    # queens = getDemoBoard()
+    heur = 0 if argv[2] == "H1" else 1
 
     if int(argv[1]) == 1:   # A* Algorithm
         pass
-    elif int(argv[1]) == 2: # Hill Climb
+    elif int(argv[1]) == 2:  # Hill Climb
         hill_solver = HillClimb(queens, heur)
         hill_solver.solve()
-    
-
 
 
 if __name__ == "__main__":
